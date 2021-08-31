2297c9255887428878dbf3e830ff9fba327bb8af
Ahmed <ahmed.abdelaliem@gmail.com>
2021-08-31 08:43:40 +0100

    add cold start log line

--- src/handlers/provisioned-concurrency-scheduled.js
+++ src/handlers/provisioned-concurrency-scheduled.js
@@ -2,6 +2,8 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+console.log('cold start');
+
 exports.scheduledEventLoggerHandler = async (event, context) => {
   await sleep(5000);
   console.info('Autoscaled Provisioned');
